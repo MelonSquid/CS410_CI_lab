04962c463f0d269bfee8e930207602e8e79d4d06
MelonSquid <nschleiff@gmail.com>
2021-06-15 16:01:30 -0700

    Added second test case, added comments on expected outcome

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,8 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -19,14 +18,18 @@ public class CILabTest {
         myString = null;
     }
 
+    //general use case -- true expected
     @Test
     public void detectCapitalUseTest1() {
         myString.setString("This is the first test String.");
         assertTrue(myString.detectCapitalUse());
     }
+
+    //general use case -- false expected
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("This is the second TEst String.");
+        assertFalse(myString.detectCapitalUse());
     }
 
 
